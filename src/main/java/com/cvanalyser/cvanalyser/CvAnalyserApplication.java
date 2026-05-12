461795dfdfe770f36ca5194d6ca74a59fb661e25
Jakub <24jakub.s@gmail.com>
2026-05-12 12:48:39 +0100

    initial project setup

---
+++ src/main/java/com/cvanalyser/cvanalyser/CvAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.cvanalyser.cvanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvAnalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvanalyser/cvanalyser/CvAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvanalyser.cvanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
